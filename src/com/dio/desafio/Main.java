43c5256ba16d235ab13c5752885fef17b19e322e
CoolHatEntity <matheus17menezes@gmail.com>
2022-02-24 22:14:56 -0300

    Mentoria e curso

--- src/com/dio/desafio/Main.java
+++ src/com/dio/desafio/Main.java
@@ -1,8 +1,32 @@
 package com.dio.desafio;
 
+import com.dio.desafio.dominio.Curso;
+import com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
+
+        curso.setTitulo("Curso java");
+        curso.setDescricao("descricao curso java");
+        curso.setCargaHoraria(1);
+
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("descricao de mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso java II");
+        curso1.setDescricao("descricao curso java II");
+        curso1.setCargaHoraria(1);
+
+        System.out.println(curso.toString());
+        System.out.println(mentoria.toString());
+        System.out.println(curso1.toString());
     }
 }
